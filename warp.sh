3d703fae4bdb06c736871709f5f2c02de3ca00ee
zmaplex <zmaplex@gmail.com>
2024-03-06 02:18:16 +0800

    updated

--- warp.sh
+++ warp.sh
@@ -262,9 +262,9 @@ Init_WARP_Client() {
     if [[ ${WARP_Client_SelfStart} != enabled || ${WARP_Client_Status} != active ]]; then
         Install_WARP_Client
     fi
-    if [[ $(warp-cli --accept-tos account) = *Missing* ]]; then
+    if [[ $(warp-cli --accept-tos  registration show) = *Missing* ]]; then
         log INFO "Cloudflare WARP Account Registration in progress..."
-        warp-cli --accept-tos register
+        warp-cli --accept-tos registration new
     fi
 }
 
@@ -284,7 +284,7 @@ Disconnect_WARP() {
 
 Set_WARP_Mode_Proxy() {
     log INFO "Setting up WARP Proxy Mode..."
-    warp-cli --accept-tos set-mode proxy
+    warp-cli --accept-tos mode proxy
 }
 
 Enable_WARP_Client_Proxy() {
